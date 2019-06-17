da490bfdb6db181541ca727783f7d64b5d6d87ba
lmjcbs <liamdavidjacobs@gmail.com>
2019-06-17 19:10:15 +0200

    02/02 FizzBuzz

--- 02_Exercises.js
+++ 02_Exercises.js
@@ -1,7 +1,7 @@
 /*
 Looping a triangle
-Write a loop that makes seven calls to console,log
-to output the following trianlge:
+Write a loop that makes seven calls to console,log to output the following trianlge:
+
 #
 ##
 ###
@@ -14,4 +14,20 @@ let triangle = '';
 for (let i = 0; i < 7; i++) {
   triangle += '#';
   console.log(triangle);
+}
+
+/*
+FizzBuzz
+Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+
+When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+*/
+for (let i = 1; i <= 100; i++) {
+  if (i % 5 === 0 && i % 3 === 0) {
+    console.log('FizzBuzz');
+  } else if (i % 5 === 0) {
+    console.log('Buzz');
+  } else if (i % 3 === 0) {
+    console.log('Fizz');
+  } else console.log(i);
 }
